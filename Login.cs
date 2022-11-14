1689986492cd7ecb96ab3c0c8a767ea1d346a12e
GeorgeHammond2002 <577175@student.belgiumcampus.ac.za>
2022-11-14 14:22:24 +0200

    Comments
    Comments added to both the Login form and FileHandler class.

--- Login.cs
+++ Login.cs
@@ -12,39 +12,49 @@ namespace PRG_282_Project
 {
     public partial class Login : Form
     {
+        //Varibles declared to be used with the write() method 
         public static string name, pass;
         public Login()
         {
             InitializeComponent();
         }
-
+        //This event is triggered when the Login button is clicked 
         private void btnLogin_Click(object sender, EventArgs e)
-        {            
+        {   
+            //Two variables are declared that receives the values entered into the textboxes 
             name = txtUsername.Text;
             pass = txtPassword.Text;
             Menu main = new Menu();
-            FileHandler fileHandler = new FileHandler();           
+            FileHandler fileHandler = new FileHandler();     
+            //If statement evaluates whether the method is true or false
             if(fileHandler.Read())
-            {
+            { 
+                //Executed if flag is true and user taken to main menu 
                 MessageBox.Show("Login Successful");
                 main.Show();
                 this.Hide();             
             }
             else
             {
+                //Executed if flag is false 
                 MessageBox.Show("Username and/or Password not found!. Sign Up?");
             }
+            //Method called to clear the text boxes 
             clearTextBox();
 
         }
+
+        //Method is called when the textboxes needs to be cleared 
         public void clearTextBox()
         {
             txtUsername.Clear();
             txtPassword.Clear();
         }
+
+        //This event is triggered when the Sign Up button is clicked 
         private void btnSignUp_Click(object sender, EventArgs e)
         {
-           
+           //The user is taken to the Sign Up form 
             SignUp signUp = new SignUp();
             signUp.Show();
             this.Hide();
